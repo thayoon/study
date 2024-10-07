5d403c82ae9d510c8bb0ac665295ad1349d29c84
thayoon <thagyun@naver.com>
2024-10-07 14:28:35 +0900

    algo: Sorting/241007

---
+++ programmers/Solution_H-index_java.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(citations[i] >= n-i) {
+                h = n-i;
+                break;
+            }
+        }
+        
+        return h;
+    }
+}
