3ccc1bcd8ee85c194e105ae0ba0ec0763c897611
thayoon <thagyun@naver.com>
2024-10-31 01:48:51 +0900

    algo: Ad-Hoc/241031

---
+++ softeer/Main_9496_java.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-1]+N-1);
+    }
+}
