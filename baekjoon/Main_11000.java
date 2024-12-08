4c8ff803eb19854afe4257070431a8001085e155
thayoon <thagyun@naver.com>
2024-12-08 15:21:32 +0900

    algo: Greedy/241208

---
+++ baekjoon/Main_11000.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_11000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        int[][] time = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            }
+            return Integer.compare(o1[0], o2[0]);
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(time[0][1]);
+
+        for (int i = 1; i < N; i++) {
+            if(pq.peek() <= time[i][0]) {
+                pq.poll();
+            }
+            pq.offer(time[i][1]);
+        }
+
+        System.out.println(pq.size());
+    }
+}
