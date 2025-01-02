1678169d8a449aa78632f8318579fc3e0488f17a
thayoon <thagyun@naver.com>
2025-01-02 13:51:53 +0900

    algo: dp/250102

---
+++ baekjoon/Main_11052.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j]+P[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
