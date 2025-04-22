c260eed37579352371884ea42499639f79d6be1d
thayoon <thagyun@naver.com>
2025-04-22 18:33:08 +0900

    algo: Greedy/250422

---
+++ baekjoon/Main_2785.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            cnt = N-i-1;
+            sum += arr[i];
+
+            if(sum >= cnt) break;
+        }
+
+        System.out.println(cnt);
+    }
+}
