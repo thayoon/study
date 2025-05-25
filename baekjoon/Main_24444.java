76d2d54bfed469b19cbf46fc70f88928d5ee6724
thayoon <thagyun@naver.com>
2025-05-25 22:49:32 +0900

    algo: bfs/250525

---
+++ baekjoon/Main_24444.java
@@ -0,0 +1,60 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_24444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] adj = new List[N+1];
+        for (int i = 0; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(adj[i]);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] visited = new int[N+1];
+        q.offer(R);
+        int cnt = 1;
+        visited[R] = cnt;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            for(int next: adj[now]) {
+                if(visited[next] == 0) {
+                    q.offer(next);
+                    cnt++;
+                    visited[next] = cnt;
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
